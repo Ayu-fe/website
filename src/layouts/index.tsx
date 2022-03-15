37c8eaf05df7238707edbd9aee677118c9740404
王永帅 <wangyongshuai.ayu@bytedance.com>
2022-03-15 11:12:04 +0800

    feat: 添加导航&header

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,5 +1,5 @@
 import { IRouteComponentProps } from 'umi'
-import { Layout } from 'antd'
+import { Layout, Menu } from 'antd'
 import style from './index.less'
 
 const { Header, Footer, Content } = Layout
@@ -11,9 +11,26 @@ export default function UmiLayout({
   history,
   match,
 }: IRouteComponentProps) {
+  const handleClick = (item: Record<string, any>) => {
+    history.push(item.key)
+  }
+
   return (
-    <Layout>
-      <Header className={style.header}>Header</Header>
+    <Layout className={style.layout}>
+      <Header className={style.header}>
+        <Menu
+          defaultSelectedKeys={['/']}
+          mode="horizontal"
+          className={style.menu}
+          onClick={handleClick}
+        >
+          <Menu.Item key="/">首页</Menu.Item>
+          <Menu.Item key="/work">技术博客</Menu.Item>
+          <Menu.Item key="/life">生活随笔</Menu.Item>
+          <Menu.Item key="/music_blog">音乐与我</Menu.Item>
+          <Menu.Item key="/brove">奇思妙想</Menu.Item>
+        </Menu>
+      </Header>
       <Content className={style.content}>{children}</Content>
       <Footer className={style.footer}>Footer</Footer>
     </Layout>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,5 +2,20 @@ import styles from './index.less'
 import { Button } from 'antd'
 
 export default function IndexPage() {
-  return <div>this is home</div>
+  return (
+    <div className={styles.container}>
+      <div className={styles.header}>
+        <img
+          src={require('../static/image/Bg.png')}
+          alt=""
+          style={{ objectFit: 'cover' }}
+          width="100%"
+          height="100%"
+        />
+      </div>
+      <div className={styles.module}></div>
+      <div className={styles.pic}></div>
+      <div className={styles.callMe}></div>
+    </div>
+  )
 }
